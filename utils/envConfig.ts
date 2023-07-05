98fb26425e3b8f720e00ebb667043b5f5c16b4b4
Melvin <nguyen.tuan.dat@moneyforward.vn>
2023-07-05 20:25:22 +0700

    update default config

--- utils/envConfig.ts
+++ utils/envConfig.ts
@@ -4,10 +4,10 @@ import path from 'path';
 configDotenv({ path: path.resolve(__dirname, '../', '.env') });
 
 const envConfig = {
-  baseUrl: process.env.BASE_URL || '',
+  baseUrl: process.env.BASE_URL || 'https://payable-invoice.test.mfw.work',
   baseUrlAPI: process.env.BASE_URL_API || '',
-  email: process.env.EMAIL || '',
-  password: process.env.PASSWORD || '',
+  email: process.env.EMAIL || 'nguyen.tuan.dat@moneyforward.vn',
+  password: process.env.PASSWORD || 'iLR0x@zPE6NT',
   storageState: process.env.STORAGE_STATE || '',
   browser: process.env.BROWSER || '',
 };
