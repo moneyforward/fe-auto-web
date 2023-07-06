7b37042f458aaf56eef1502f29e6f641a66cf77e
Melvin <nguyen.tuan.dat@moneyforward.vn>
2023-07-06 08:26:59 +0700

    debug

--- tests/support/config.ts
+++ tests/support/config.ts
@@ -18,7 +18,7 @@ const browserOptions: LaunchOptions = {
 export const envConfig = {
   email: process.env.EMAIL || 'nguyen.tuan.dat@moneyforward.vn',
   password: process.env.PASSWORD || 'iLR0x@zPE6NT',
-  baseUrl: process.env.BASE_URL || 'https://payable-invoice.test.mfw.work',
+  baseUrl: process.env.BASE_URL || 'https://receivable.mfvn.dev',
   baseUrlAPI: process.env.BASE_URL_API || '',
 };
 
